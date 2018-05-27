a44d9ea27a0e6fff56b61fc667055c70adae6584
jesod151 <32991773+jesod151@users.noreply.github.com>
2018-05-27 14:54:57 -0600

    Prueba

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	printf("hola Mundo \n");
+	printf("hola \n");
+	
 	return 0;
 }
