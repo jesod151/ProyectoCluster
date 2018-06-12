93db9829fc91cb3a13079d1dfb793417fe3c6ab3
jesod151 <32991773+jesod151@users.noreply.github.com>
2018-06-11 18:39:15 -0600

    Algoritmo final
    se adaptó el algoritmo aprobado por el profesor a opemp y se logró paralelizar

--- main.cpp
+++ main.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <time.h>
+#include <omp.h>
 #include <stdio.h>
+#define MAX 100
 
 using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	printf("Hola Mundo Prueba!\n");
-	
-	return 0;
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   bool swapped;
+   
+   //omp_set_num_threads(1);
+   //#pragma omp parallel for private(i, j, swapped)
+   for (i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
+        {
+           swap(&arr[j], &arr[j+1]);
+           swapped = true;
+        }
+     }
+   }
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    clock_t tiempo = clock();
+    int n = 200000;
+    int arr[n];
+    
+    srand (time(NULL));
+
+	for(int i = 0; i < n; i++)
+		arr[i] = rand() % 1000;
+
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    printf("tiempo es: %ld \n",(clock() - tiempo)/CLOCKS_PER_SEC);
+
+    return 0;
 }
